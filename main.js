8d9463656ce22c43d16fed005b1bbfa901944c7b
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 08:08:35 -0700

    Q 22 TS/JS for 45 assignment

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//   Intentional Error: If you haven't recived an array index error in one og your
+// programs yet , try to make one happen. Charge an index in one of your programs
+//produce an index error. Make sure you correct the error before closing the program.
+const days = ["Sunday", "monday", "thuesday", "wednesday", "thursday", "friday", "saturday"];
+//console.log(days{7})
+console.log(days[6]);
---
+++ main.ts
@@ -0,0 +1,8 @@
+//   Intentional Error: If you haven't recived an array index error in one og your
+// programs yet , try to make one happen. Charge an index in one of your programs
+//produce an index error. Make sure you correct the error before closing the program.
+
+const days : string[]= ["Sunday","monday","thuesday","wednesday","thursday","friday","saturday"]
+
+//console.log(days{7})
+console.log(days[6])
